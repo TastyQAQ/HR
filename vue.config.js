80aaefe08aca578d4ac4d2b3ad25aa42cdc0acf7
TastyQAQ <aq22171280@gmail.com>
2024-02-01 20:35:42 +0800

    更改配置

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: './',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
@@ -39,11 +39,7 @@ module.exports = {
     proxy: {
       '/api': {
         target: 'https://heimahr.itheima.net'
-      },
-      '/prod:api': {
-        target: 'https://heimahr-t.itheima.net'
       }
-
     }
     // before: require('./mock/mock-server.js')
   },
